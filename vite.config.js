2c987b9ba9132512ac9676d876deae30397571f3
Ezequiel Ferreira <helloworld.gogam@gmail.com>
2025-07-15 16:54:59 -0400

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< Updated upstream
-  base: "manga_reader_vue",
-=======
   base: "/manga_reader_vue/",
->>>>>>> Stashed changes
   plugins: [vue()],
 })
