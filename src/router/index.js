dc6e41300f70c2960427700dfe98918cb72e6192
Ezequiel Ferreira <helloworld.gogam@gmail.com>
2025-07-15 16:57:56 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Index from '../components/Index.vue';
+import Serie from '../components/Serie.vue';
+import Leer from '../components/Leer.vue';
+
+const routes = [
+  { path: '/', component: Index },
+  { path: '/serie/:id', component: Serie },
+  { path: '/leer/:id/:cap?', component: Leer },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // ✅ OK
-  routes: [
-    // tus rutas
-  ]
+  history: createWebHistory("/manga_reader_vue"),
+  routes,
 });
-
 export default router;
