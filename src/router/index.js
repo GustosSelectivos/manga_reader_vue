a2f7743a61162d2dd240d752dd8b500f56cbcae9
Ezequiel Ferreira <helloworld.gogam@gmail.com>
2025-07-15 17:19:26 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,15 @@ import Serie from '../components/Serie.vue';
 import Leer from '../components/Leer.vue';
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', component: Index },  // Cambié Home por Index
   { path: '/serie/:id', component: Serie },
   { path: '/leer/:id/:cap?', component: Leer } // capítulo opcional
-]
-
+];
 
 const router = createRouter({
   history: createWebHistory("/manga_reader_vue"),
   routes,
 });
+
 export default router;
+
